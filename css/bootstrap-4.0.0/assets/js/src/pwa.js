fc222631699e2111e05ef7ab6b90bd688d434480
SchulichIgnite <54962971+SchulichIgnite@users.noreply.github.com>
2020-10-10 10:14:36 -0600

    Minified 73 files

---
+++ css/bootstrap-4.0.0/assets/js/src/pwa.js
@@ -0,0 +1,1 @@
+!function(){"use strict";"serviceWorker"in navigator?window.addEventListener("load",(function(){navigator.serviceWorker.register("/sw.js").then((function(e){console.log("ServiceWorker registration successful with scope: ",e.scope)})).catch((function(e){console.log("ServiceWorker registration failed: ",e)}))})):console.log("Service workers are not supported.")}();
