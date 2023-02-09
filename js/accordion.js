24b55b98807ad6354401565bb239188ad3624c4d
Descent098 <Descent098@users.noreply.github.com>
2023-02-09 03:37:46 +0000

    Publishing Site schulichignite.com at dd91cfa8aa5d6793921aa141e06113e7ecc310e3 on Thu Feb  9 03:37:46 UTC 2023

--- js/accordion.js
+++
@@ -1,9 +0,0 @@
-/** This function opens and closes the accordions */
-function openAnswer(content) {
-  var x = content;
-  if (x.className.indexOf("hide") == -1) {
-    x.className += " hide";
-  } else { 
-    x.className = x.className.replace(" hide", " ");
-  }
-}
