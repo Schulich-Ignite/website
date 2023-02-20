f948f270740013b414e60f7dd3b9bfc680111939
Kieran Wood <kieranw098@gmail.com>
2023-02-19 22:11:36 -0700

    Added necessary purgeCSS files

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+// This file configures how our purgeCSS system runs: https://purgecss.com/guides/hugo.html
+const purgecss = require("@fullhuman/postcss-purgecss")({
+    content: ["./hugo_stats.json"],
+    defaultExtractor: (content) => {
+      const els = JSON.parse(content).htmlElements;
+      return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+    },
+    safelist: [],
+  });
+  
+  module.exports = {
+    plugins: [
+      purgecss
+    ],
+  };
